06eed2de95766a174f18d5588e8014feb171ac56
davin-lott <legodav42@gmail.com>
2025-03-28 20:10:31 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,100 @@
+/*Here are my reflections comments.
+This was a hard project. I learned a lot, especially about how CSS actually works.
+I wish I could have started sooner, but I am happy with what I got done.
+My biggest regret is that I didn't have more content in my website, but I am satisfied with the CSS,
+Javascript, and HTML that I was able to put together. I think it all came together pretty well. I hope 
+the time and effort that I put in shows in this
+*/
+
+let onOrOff = false
+
+function changeDaTheme(){let newSheet = document.createElement("link")
+    if (onOrOff == false){
+        let newSheet = document.createElement("link")
+        newSheet.rel = "stylesheet"
+        newSheet.href = "secondCss.css"
+        newSheet.style.id="secondSheet"
+        document.getElementsByTagName("head")[0].appendChild(newSheet)
+        onOrOff = true
+    } else{
+        let myHead = document.getElementsByTagName("head")[0]
+        myHead.removeChild(myHead.lastChild)
+        onOrOff = false
+    }
+
+}
+
+
+function changeDisplayOn(daClassName, newAttribute){
+    document.getElementsByClassName(daClassName).item(0).style.display = newAttribute
+}
+
+function changeDisplayOff(daClassName){
+    document.getElementsByClassName(daClassName).item(0).style.display = "none"
+}
+
+function clickedThis(daClassName){
+    if (daClassName == "mainpage"){
+        changeDisplayOn("mainpage", "grid")
+        changeDisplayOff("contactpage")
+        changeDisplayOff("questionpage")
+    }
+    if(daClassName == "contactpage" ){
+        changeDisplayOff("mainpage")
+        changeDisplayOn("contactpage", "grid")
+        changeDisplayOff("questionpage")
+    }
+    if(daClassName == "questionpage"){
+        changeDisplayOff("mainpage")
+        changeDisplayOff("contactpage")
+        changeDisplayOn("questionpage", "grid")
+    }
+}
+
+
+function clickmain(){
+    clickedThis("mainpage")
+}
+
+function clickcontact(){
+    clickedThis("contactpage")
+}
+
+function clickquestion(){
+    clickedThis("questionpage")
+}
+
+
+
+
+
+/*function changeDisplayOn(daClassName){
+    document.getElementsByClassName(daClassName).item(0).style.display = "inline-block"
+}
+
+function changeDisplayOff(daClassName){
+    document.getElementsByClassName(daClassName).item(0).style.display = "none"
+}
+
+function clickedThis(daClassName){
+    if (daClassName == "")
+}
+
+
+function clickmain(){
+    changeDisplayOn("mainpage")
+    changeDisplayOff("contactpage")
+    changeDisplayOff("questionpage")
+}
+
+function clickcontact(){
+    changeDisplayOff("mainpage")
+    changeDisplayOn("contactpage")
+    changeDisplayOff("questionpage")
+}
+
+function clickquestion(){
+    changeDisplayOff("mainpage")
+    changeDisplayOff("contactpage")
+    changeDisplayOn("questionpage")
+}*/
